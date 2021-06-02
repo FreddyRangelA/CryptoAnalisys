03210f78a7b3a4a473ed2d1718daabc58c455313
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-06-01 19:22:05 -0500

    update for flask

--- app.py
+++ app.py
@@ -8,31 +8,15 @@ from flask import g
 
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
 
-
-#mongo = PyMongo(app, uri="mongodb://localhost:27017/project-2_db")
-#Database='crypto_df.db'
-
-# Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
-    # # Find one record of data from the mongo database
-    #data={} 
-    #destination_data=Database
-
-
-    # # Return template and data
     return render_template("index.html")
     
-#S@app.route("/livePrice")
+#S@app.route("/")
 #def livePrice():
-    #data = priceLive.livePrice()
-    #mongo.db.collection.insert(data)
-    # use mongo update to upsert data
-    #mongo.db.collection.update({},data, upsert=True)
-    #return redirect("/", code=302)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
